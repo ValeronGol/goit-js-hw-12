b2810c8e7aec61f9ea407524633eaf23961fd678
Valerit Golovatenko <valeras7777@gmail.com>
2021-07-16 20:25:35 +0300

    создание поиска стран

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
 import './css/styles.css';
+import API from './js/fetchCountries';
+import Notiflix from "notiflix";
+import _ from 'lodash';
+import countryCard from './templates/country.hbs';
+import countriesList from './templates/countriesList.hbs'
+
+
 
 const DEBOUNCE_DELAY = 300;
+const searchBox = document.getElementById('search-box');
+const countryInfo = document.querySelector('.country-info');
+const countryList = document.querySelector('.country-list');
+
+searchBox.addEventListener('input',_.debounce(onSearch, DEBOUNCE_DELAY));
+
+function onSearch(event){
+  event.preventDefault();
+    const countryName = event.target.value
+API.fetchCountries(countryName)
+    .then(response => {
+      if (response.length > 10) {
+            Notiflix.Notify.info('Too many matches found. Please enter a more specific name.'); 
+      } else if (response.length === 1) {
+        renderCountry(response[0]);
+      } else{
+        renderCountriesList(response);
+      }
+      
+    })
+    .catch(error => {
+      console.log(error);
+      Notiflix.Notify.failure('Oops, there is no country with that name');
+    })
+    .finally(clearContainer());
+}
+
+
+function renderCountry(country) {
+  const lang = country.languages.map(l => l.name).join(', ');
+  console.log(lang);
+  const markup = countryCard({country, lang});
+  countryInfo.insertAdjacentHTML('beforeend', markup);
+  }
+  function renderCountriesList(countriesItem) {
+    const markup = countriesList(countriesItem);
+    countryList.insertAdjacentHTML('beforeend', markup);
+  }
+  
+  function clearContainer() {
+    countryInfo.innerHTML = '';
+    countryList.innerHTML = '';
+  }
